b24e6552ebec56e18fd41dc00a406f12112f75bc
Doh Kim <tank2705@gmail.com>
2017-05-24 18:55:50 +0000

    finished action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -60,7 +60,7 @@ class PortfoliosController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_portfolio
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
   get 'contact', to: 'pages#contact'
-
   get 'about', to: 'pages#about'
   
-  resources :blogs
+  resources :blogs do
+    member do 
+      get :toggle_status
+    end
+  end
 
 root to: 'pages#home'
 end
