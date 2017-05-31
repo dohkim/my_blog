ab28d1bfca02e45409bb2f5b0fa2da994b91cf56
Doh Kim <tank2705@gmail.com>
2017-05-31 20:35:24 +0000

    Customized devise routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  Rails.env.development? config.action_mailer.default_url_options = { host: 'https://rails-my-blog-dohkim.c9users.io/', port: 8080 }
 end
