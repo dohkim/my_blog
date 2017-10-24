414968308295aa1d1c1153b8a3ca99c43e80edd0
Doh Kim <tank2705@gmail.com>
2017-10-24 01:29:45 +0000

    Updated auth to include draft and published status tuggle controls

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
-  access all: [:show, :index], user: {except: [:destroy, :edit, :create, :new, :update]}, site_admin: :all
+  access all: [:show, :index], user: {except: [:destroy, :edit, :create, :new, :update, :toggle_status]}, site_admin: :all
   layout "blog"
 
   # GET /blogs
