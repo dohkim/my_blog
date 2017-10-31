48ab1a9ae17880e7783ea1c03a2441ef065ceb3a
Doh Kim <tank2705@gmail.com>
2017-10-31 04:36:38 +0000

    Updated Form style for portfolio

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,11 @@
 module PortfoliosHelper
+    def login_helper style = ''
+        if current_user.is_a?(GuestUser)
+            (link_to "Register", new_user_registration_path, class: style) + 
+            "<br>".html_safe + 
+            (link_to "Login", new_user_session_path, class: style)
+        else
+            link_to "Logout", destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?'}, class: style 
+        end 
+    end
 end
