a3fdd95d49e4b3bdf94ff7279981249a8aab9df9
Doh Kim <tank2705@gmail.com>
2017-11-01 05:21:32 +0000

    Impelemented refactor navigation and added active status on nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,13 @@
 module ApplicationHelper
     def login_helper style = ''
         if current_user.is_a?(GuestUser)
-            (link_to "Register", new_user_registration_path, class: style) + 
+            (link_to "Register", new_user_registration_path, class: style) +
             " ".html_safe + 
             (link_to "Login", new_user_session_path, class: style)
         else
             link_to "Logout", destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?'}, class: style 
         end 
+     
     end
     
     def source_helper(layout_name)
@@ -20,4 +21,37 @@ module ApplicationHelper
    def copyright_generator
        DohKimTool::Renderer.copyright "Doh Kim", "All rights reserved"
    end
+   
+    def nav_items
+        [
+            {url: root_path,
+                title: "Home"
+            },
+            {url: about_path,
+                title: "About Me"
+            },
+            {url: contact_path,
+                title: "Contact"
+            },
+            {url: blogs_path,
+                title: "Blog"
+            },
+            {url: portfolios_path,
+                title: "Portfolio"
+            }
+        ]
+    end
+   
+    def nav_helper style, tag_type
+        nav_links =''
+        nav_items.each do |item|
+            nav_links << "<#{tag_type}> <a href='#{item[:url]}' class='#{style} #{nav_active? item[:url]}'>#{item[:title]} </a></#{tag_type}>"
+        end
+        
+        nav_links.html_safe
+    end
+    
+    def nav_active? path
+        "active" if current_page? path
+    end
 end
--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -2,7 +2,7 @@ module PortfoliosHelper
     def login_helper style = ''
         if current_user.is_a?(GuestUser)
             (link_to "Register", new_user_registration_path, class: style) + 
-            "<br>".html_safe + 
+                " ".html_safe +
             (link_to "Login", new_user_session_path, class: style)
         else
             link_to "Logout", destroy_user_session_path, method: :delete, data: { confirm: 'Are you sure?'}, class: style 
