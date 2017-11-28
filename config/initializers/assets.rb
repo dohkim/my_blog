2e088e98f7a98657e5bbe026b33f539d3df29d89
Doh Kim <tank2705@gmail.com>
2017-11-28 04:16:46 +0000

    integragted background for app-layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( blogs.css )
-Rails.application.config.assets.precompile += %w( portfolios.css )
+Rails.application.config.assets.precompile += %w( blogs.scss )
+Rails.application.config.assets.precompile += %w( portfolios.scss )
