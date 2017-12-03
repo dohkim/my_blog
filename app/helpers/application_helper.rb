e1eda4d93efde907ec387fdb744490f300af96f2
Doh Kim <tank2705@gmail.com>
2017-12-03 06:51:20 +0000

    Refactor alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,4 +54,18 @@ module ApplicationHelper
     def nav_active? path
         "active" if current_page? path
     end
+    
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+        
+        if alert
+              alert_generator alert
+        end
+    end
+    
+    
+    def alert_generator msg
+        js add_gritter(msg, title: "Doh kim's Blog", sticky: false)
+    end
+    
 end
