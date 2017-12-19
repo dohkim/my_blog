88aaa40b3aebd93ab810c01a8b159d578ad83891
Doh Kim <tank2705@gmail.com>
2017-12-19 18:14:14 +0000

    Fixed JS loading bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery
-//= require tether
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
@@ -21,3 +20,4 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+//= require tether
